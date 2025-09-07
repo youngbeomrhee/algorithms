addac9444901120547e7ac28769f7dfcf33dd0b8
minenam <nhm6947@gmail.com>
2025-09-08 00:41:12 +0900

    chore: 1주차 정확한 시간 복잡도 주석 추가

--- 2025-season6/1주차/남혜민/best-time-to-buy-and-sell-stock.ts
+++ 2025-season6/1주차/남혜민/best-time-to-buy-and-sell-stock.ts
@@ -3,7 +3,7 @@
 // 1) 현재까지의 최저 매수가격 비교
 // 2) 현재 가격에서 팔 때의 최대 이익을 계산
 
-// 전체 시간 복잡도: O(n)
+// 전체 시간 복잡도: O(n-1) => O(n)
 // - 배열을 한 번만 순회하면서 최저가와 최대 이익을 갱신
 // - n은 prices 배열의 길이
 function maxProfit1(prices: number[]): number {
@@ -11,15 +11,14 @@ function maxProfit1(prices: number[]): number {
     let maxProfit = 0;
 
     for (let i = 1; i < prices.length; i++) {
+        // O(n) - n-1번 반복
         const currentPrice = prices[i];
         if (currentPrice < minPrice) {
-            // 지금 기준 최저 매수가격 비교
             minPrice = currentPrice;
         }
 
         const todayPrice = currentPrice - minPrice;
         if (todayPrice > maxProfit) {
-            // 지금 기준 최대 이익 가격
             maxProfit = todayPrice;
         }
     }
